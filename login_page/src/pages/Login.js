4e5a13a6c9f81c35571aaee661a8cf5ecd5bed48
Scott-Zeta <wc070211@gmail.com>
2022-09-05 20:17:04 +0930

    one input box, end the day

--- login_page/src/pages/Login.js
+++ login_page/src/pages/Login.js
@@ -1,6 +1,33 @@
+import { StyledTextInput, StyledFormArea, StyledFormButton, StyledLabel, Avatar, StyledTitle, colors } from './../components/Styles'
+
+import Logo from './../assets/logo.svg';
+
+//formik
+import { Formik, Form } from 'formik'
+import { TextInput } from '../components/FormLib';
+
 const Login = () => {
-    return(
-        <p>Login</p>
+    return (
+        <div>
+            <StyledFormArea>
+                <Avatar image={Logo} />
+                <StyledTitle color={colors.theme} size={30}>
+                    Member Login
+                </StyledTitle>
+                <Formik>
+                    {() => (
+                        <Form>
+                            <TextInput 
+                                name = "email"
+                                type = "text"
+                                label = "Email Address"
+                                placeholde = "address@example.com"
+                            />
+                        </Form>
+                    )}
+                </Formik>
+            </StyledFormArea>
+        </div>
     )
 }
 
