75881c027fc836246de0fccb6f901de70ef3c2f6
Scott-Zeta <wc070211@gmail.com>
2022-09-06 15:49:45 +0930

    show and hide password

--- login_page/src/components/FormLib.js
+++ login_page/src/components/FormLib.js
@@ -1,35 +1,45 @@
-import {useState} from 'react';
-import {useField} from 'formik';
-import{
+import { useState } from 'react';
+import { useField } from 'formik';
+import {
     StyledTextInput,
     StyledLabel,
     StyledIcon
-}from "./../components/Styles";
+} from "./../components/Styles";
 
 //password eye icon
 import { FiEye, FiEyeOff } from 'react-icons/fi';
 
-export const TextInput = ({icon, ...props}) => {
+export const TextInput = ({ icon, ...props }) => {
     const [field, meta] = useField(props);
     const [show, setShow] = useState(false);
 
-    return(
-        <div style={{position: "relative"}}>
+    return (
+        <div style={{ position: "relative" }}>
             <StyledLabel htmlFor={props.name}>
                 {props.label}
             </StyledLabel>
-            <StyledTextInput 
+            {props.type !== "password" && (<StyledTextInput
                 {...field}
                 {...props}
             />
+            )}
+
+            {props.type === "password" && (
+                <StyledTextInput
+                    {...field}
+                    {...props}
+                    type={show ? "text" : "password"}
+                />
+            )}
+
             <StyledIcon>
                 {icon}
             </StyledIcon>
             {
                 props.type === "password" &&
                 <StyledIcon onClick={() => setShow(!show)} right>
-                    {show && <FiEye/>}
-                    {!show && <FiEyeOff/>}
+                    {show && <FiEye />}
+                    {!show && <FiEyeOff />}
                 </StyledIcon>
             }
         </div>
