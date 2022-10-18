fa55c38aed35aaf6fcaf31f4b6fb8be753d2ac53
Scott-Zeta <wc070211@gmail.com>
2022-10-18 17:53:59 +1030

    input validation check

--- TP_ST/login_server/api/User.js
+++ TP_ST/login_server/api/User.js
@@ -3,7 +3,42 @@ const router = express.Router()
 
 //signup
 router.post('/signup', (req,res) =>{
+    let {name, email, password, dateofBirth} = req.body;
+    //trim for remove whithe space
+    name = nmae.trim();
+    email = email.trim();
+    password = password.trim();
+    dateofBirth = dateofBirth.trim();
 
+    if(name == "" || email == "" || password == "" || dateofBirth == ""){
+        res.json({
+            status: "FAILED",
+            message: "Empty fields!"
+        })
+    }else if(!/^[a-zA-Z]*$/.test(name)){
+        res.json({
+            status: "FAILED",
+            message: "Invalid name"
+        })
+    }else if(!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)){
+        res.json({
+            status: "FAILED",
+            message: "Invalid email"
+        })
+    }else if (!new Date(dateofBirth).getTime()){
+        res.json({
+            status: "FAILED",
+            message: "Invalid date of birth"
+        })
+    }else if(password.lenth < 8){
+        res.json({
+            status: "FAILED",
+            message: "Password too short"
+        })
+    } else {
+        //check if user already exists
+        
+    }
 })
 
 //login
