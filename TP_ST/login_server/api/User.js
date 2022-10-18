8cbfea47feb7f0fb19209c85d1ae02c5f9155d12
Scott-Zeta <wc070211@gmail.com>
2022-10-18 18:28:14 +1030

    hashing the password

--- TP_ST/login_server/api/User.js
+++ TP_ST/login_server/api/User.js
@@ -4,52 +4,85 @@ const router = express.Router()
 //mongodb user model
 const User = require('./../models/User');
 
+//Password handler
+const bcrypt = require('bcrypt');
+
 //signup
-router.post('/signup', (req,res) =>{
-    let {name, email, password, dateofBirth} = req.body;
+router.post('/signup', (req, res) => {
+    let { name, email, password, dateofBirth } = req.body;
     //trim for remove whithe space
     name = nmae.trim();
     email = email.trim();
     password = password.trim();
     dateofBirth = dateofBirth.trim();
 
-    if(name == "" || email == "" || password == "" || dateofBirth == ""){
+    if (name == "" || email == "" || password == "" || dateofBirth == "") {
         res.json({
             status: "FAILED",
             message: "Empty fields!"
         })
-    }else if(!/^[a-zA-Z]*$/.test(name)){
+    } else if (!/^[a-zA-Z]*$/.test(name)) {
         res.json({
             status: "FAILED",
             message: "Invalid name"
         })
-    }else if(!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)){
+    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
         res.json({
             status: "FAILED",
             message: "Invalid email"
         })
-    }else if (!new Date(dateofBirth).getTime()){
+    } else if (!new Date(dateofBirth).getTime()) {
         res.json({
             status: "FAILED",
             message: "Invalid date of birth"
         })
-    }else if(password.lenth < 8){
+    } else if (password.lenth < 8) {
         res.json({
             status: "FAILED",
             message: "Password too short"
         })
     } else {
         //check if user already exists
-        User.find({email}).then(result =>{
-            if(result.length){
+        User.find({ email }).then(result => {
+            if (result.length) {
                 //user already exists
                 res.json({
                     status: "FAILED",
                     message: "User with the provide email already exists"
                 })
-            }else{
+            } else {
                 //try create new user
-                
+
+                //password handling
+                const saltRounds = 10;
+                bcrypt.hash(password, saltRounds).then(hashedPassword => {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password: hashedPassword,
+                        dateofBirth
+                    });
+
+                    newUser.save().then(result => {
+                        res.json({
+                            status: "SUCCESS",
+                            message: "Signup successful!",
+                            data: result,
+                        })
+                    })
+                        .catch(err => {
+                            res.json({
+                                status: "FAILED",
+                                message: "error occurred while Saving the User Account"
+                            })
+                        })
+                })
+                    .catch(err => {
+                        res.json({
+                            status: "FAILED",
+                            message: "error occurred while hashing password"
+                        })
+                    })
             }
         }).catch(err => {
             console.log(err);
