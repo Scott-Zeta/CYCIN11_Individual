027cf51efb0723d4e2b37fc4b41f2220830509d9
Scott-Zeta <wc070211@gmail.com>
2022-10-18 14:25:29 +1030

    initial login backend

--- TP_ST/src/auth/actions/userActions.js
+++ TP_ST/src/auth/actions/userActions.js
@@ -1,8 +1,19 @@
-export const loginUser = (credentials, history,setFiledError,setSubmitting) => {
+export const loginUser = (credentials, history, setFiledError, setSubmitting) => {
+    //Make checks and get some data
+    const user = {
+        name: "Zeta",
+        "email": "wc@gmail.com"
+    }
+    const status = true;
 
+    if (status === true) {
+        //allow
+    } else {
+        //deny
+    }
 }
 
-export const signupUser = (credentials, history,setFiledError,setSubmitting) => {
+export const signupUser = (credentials, history, setFiledError, setSubmitting) => {
 
 }
 
