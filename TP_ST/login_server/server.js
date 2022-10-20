2e7088f51679c0924710e4bee6b3ebd8c3c8463b
Scott-Zeta <wc070211@gmail.com>
2022-10-20 14:11:49 +1030

    setting axious

--- TP_ST/login_server/server.js
+++ TP_ST/login_server/server.js
@@ -2,7 +2,11 @@
 require('./config/db');
 
 const app = require('express')();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+
+//cors
+const cors = require("cors");
+app.use(cors());
 
 const UserRouter = require('./api/User');
 
--- TP_ST/src/auth/actions/userActions.js
+++ TP_ST/src/auth/actions/userActions.js
@@ -1,5 +1,20 @@
+import axios from 'axios';
+
 export const loginUser = (credentials, history, setFiledError, setSubmitting) => {
     //Make checks and get some data
+    axios.post("https://evening-woodland-35500.herokuapp.com/user/signin",
+    credentials,
+    {
+        headers:{
+            "Context-Type": "application/json"
+        }
+    }
+    ).then((response) =>{
+        //take out data from the response
+        
+    }).catch(err => console.error(err))
+
+
     const user = {
         name: "Zeta",
         "email": "wc@gmail.com"
