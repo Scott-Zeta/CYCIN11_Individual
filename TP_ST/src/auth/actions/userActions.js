c5c1a09986b390f44d661bf2d557131d4db77581
Scott-Zeta <wc070211@gmail.com>
2022-10-20 14:46:08 +1030

    login success, bug fix

--- TP_ST/src/auth/actions/userActions.js
+++ TP_ST/src/auth/actions/userActions.js
@@ -1,31 +1,46 @@
 import axios from 'axios';
 
-export const loginUser = (credentials, history, setFiledError, setSubmitting) => {
+import { sessionService } from 'redux-react-session';
+
+export const loginUser = (credentials, history, setFieldError, setSubmitting) => {
     //Make checks and get some data
     axios.post("https://evening-woodland-35500.herokuapp.com/user/signin",
-    credentials,
-    {
-        headers:{
-            "Context-Type": "application/json"
+        credentials,
+        {
+            headers: {
+                "Context-Type": "application/json"
+            }
         }
-    }
-    ).then((response) =>{
+    ).then((response) => {
         //take out data from the response
-        
+        const { data } = response;
+        if (data.status === "FAILED") {
+            const { message } = data;
+
+            //check for specific error
+            if (message.includes("credentials")) {
+                setFieldError("email", message);
+                setFieldError("password", message);
+            } else if (message.includes("password")) {
+                setFieldError("password", message);
+            }
+        } else if (data.status === "SUCCESS") {
+            const userData = data.data[0];
+
+            const token = userData._id;
+
+            sessionService.saveSession(token).then(() => {
+                sessionService.saveUser(userData).then(() => {
+                    //history.push("/dashboard");
+                    history("/dashboard")
+                }).catch(err => console.error(err))
+            }).catch(err => console.error(err))
+        }
+        //complete submission
+        setSubmitting(false);
+
     }).catch(err => console.error(err))
 
-
-    const user = {
-        name: "Zeta",
-        "email": "wc@gmail.com"
-    }
-    const status = true;
-
-    if (status === true) {
-        //allow
-    } else {
-        //deny
-    }
 }
 
 export const signupUser = (credentials, history, setFiledError, setSubmitting) => {
